65c903b9ea03960cd8c61d440cd792bc16970077
Bogdan <bonakulik@gmail.com>
2021-02-02 22:14:16 +0300

    Improved Stonie

---
+++ Assets/Scripts/NPCAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAnimation : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.speed = 1f;
+        anim.speed = anim.GetFloat("Animation Speed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
